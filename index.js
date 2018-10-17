5163974f2977e71c9cd953fbcc1b62bc8361943a
Heidi Engstrom <hengstrom25@gmail.com>
2018-10-17 19:01:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,17 @@ function distanceFromHqInFeet(blocks) {
 function distanceTravelledInFeet(start, end) {
   
 }
+
+  describe('distanceTravelledInFeet()', function() {
+    it('returns the distance travelled in feet', function() {
+      expect(distanceTravelledInFeet(43, 48)).to.equal(1320);
+    });
+
+    it('returns a distance in feet', function() {
+      expect(distanceTravelledInFeet(50, 60)).to.equal(2640);
+    });
+
+    it('returns distance when destination is below distance', function() {
+      expect(distanceTravelledInFeet(34, 28)).to.equal(1584);
+    });
+  });

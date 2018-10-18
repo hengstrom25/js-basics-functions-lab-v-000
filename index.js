a6e5ac5bef3f1e576c2eb8e264328dd967a1130f
Heidi Engstrom <hengstrom25@gmail.com>
2018-10-18 18:38:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function calculatesFarePrice(start, destination) {
     if (distance <= 400)
       return 0;
     else if (distance =< 400 && distance <= 200)
-      return 
+      return .02 * (distance - 400);
   
   
 }
